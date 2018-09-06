43ff1274322f89d6d81935f3642b147f02d35cfa
Marcin <elomelo302@gmail.com>
2018-09-06 13:19:58 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('#header').click(function(){
+        $("#aboutMe").animate({ scrollTop: $('#aboutMe').height() + 1000 }, 40000);
+    });
+    for(i=0;i<400;i++) {
+        let left = Math.random() * 2000;
+        let top = Math.random() * 1000;
+                
+        star = '<div class=\'star\' style=\' top:'+ top +'px; left:'+ left +'px; \'  ></div>'
+        
+        $('#starsBackground').append( star )
+    }
+    
+    $('#starsBackground, .menu').mousemove(function(event) {
+    
+        var x = event.clientX;
+        var y = event.clientY;
+                            
+        
+        for(i=0;i<$('.star').length;i++) {
+            if ( y - parseInt( $('.star')[i].style.top ) < 150 &&  parseInt( $('.star')[i].style.top ) - y < 150 && x - parseInt( $('.star')[i].style.left ) < 150 && parseInt( $('.star')[i].style.left ) - x < 150) {
+                $('.star')[i].style.opacity = '1'				
+            }
+            else { 
+                $('.star')[i].style.opacity = '0'
+            }
+        }
+    })    
+});			
